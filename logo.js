9a05394f7563e3596f8f02cc645f5c5dacce16d6
David George <72946661+StudyGrid@users.noreply.github.com>
2023-10-21 20:02:22 -0400

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.querySelector(".logo-container").style.display = "none";
-    window.location.href = "index.html"; // Redirect to the main content page
+    window.location.href = "home.html"; // Redirect to the main content page
   }, 3000); // Wait for 1 second (1000 milliseconds) before redirecting
 });
